78a22fe9f85134d2f01253bd91115aacb7194420
nisino25 <nisino25@gmail.com>
2021-04-17 19:52:25 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="notion-navbar">
+    <a class="notion-link notion-breadcrumb__item single" >
+      <!-- <div class="notion-navbar__title">Nozomu Ando</div> -->
+      <span class="notion-navbar__title">Nozomu Ando</span>
+    </a>
+  </div>
+
+  <!-- <div style="backgroundColor:red; height: 10000px">jeuy</div> -->
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  html {
+    font-size: 16px;
+  }
+
+  
+
+  body {
+    color: var(--color-text-default);
+    background-color: var(--color-bg-default);
+    fill: currentColor;
+  }
+
+  body {
+    font-family: Quicksand, "Helvetica","Apple Color Emoji","Segoe UI Emoji",NotoColorEmoji, "Noto Color Emoji","Segoe UI Symbol","Android Emoji",EmojiSymbols, -apple-system,"BlinkMacSystemFont","Segoe UI","Roboto", "Helvetica Neue","Noto Sans",sans-serif;
+  }
+
+  .notion-navbar {
+    box-shadow: 7px 7px 10px rgb(0 0 0 / 10%), -7px -7px 10px rgb(0 0 0 / 10%);
+  }
+
+  .notion-navbar {
+    height: 45px;
+    padding: 0 14px;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: flex;
+    -webkit-align-items: center;
+    -moz-box-align: center;
+    align-items: center;
+    background-color: #fff;
+    width: 100%;
+    position: -webkit-sticky;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+    background-color: var(--color-bg-default);
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
+
+// import Vue from 'vue'
+// import BootstrapVue from "bootstrap-vue"
+// import App from './App.vue'
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap-vue/dist/bootstrap-vue.css"
+
+// Vue.use(BootstrapVue)
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
